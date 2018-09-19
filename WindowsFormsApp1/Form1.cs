fe8894b0c5dace4bc3e618ecafdfe0c77dad900b
MichaJ <michajanssen93@gmail.com>
2018-09-19 07:41:30 +0200

    Commit #001
    Added initial form keyHandler.
    
    - Form 1 / 2 (inventory)

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        Form form2 = new Form2();
+        public Form1()
+        {
+            InitializeComponent();
+            form2.Hide();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("mFrame Loading form2(inventory)");
+            form2.Hide();
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.I)
+            {
+                if (form2.Visible)
+                {
+                    form2.Show();
+                }
+                else
+                {
+                    form2.Hide();
+                }
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp1/Inventory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Inventory
+{
+    public ItemDB DB;
+    public List<Item> inventory = new List<Item>();
+    public List<Item> slots = new List<Item>();
+    public int slotCount = slots.Count;
+    private bool showToolTip;
+    private String toolTip;
+
+	public Inventory()
+	{
+        for (int i = 0; inventory.Count; i++)
+        {
+            slots.add(new Item());
+        }
+
+	}
+
+
+}
